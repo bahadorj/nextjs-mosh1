c6567229ae7749996746f8e75b11e0f8ac38c6e0
bahadorj <bahador.jorjani@gmail.com>
2025-05-10 12:57:07 -0400

    2-8

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,10 @@
+"use client";
+const AddToCart = () => {
+  return (
+    <>
+      <button onClick={() => console.log("click!")}>Add to Cart</button>
+    </>
+  );
+};
+
+export default AddToCart;
