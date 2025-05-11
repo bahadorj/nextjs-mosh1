b3c3a01655ad48b6a6a83e49a27bd4508a87cdb2
bahadorj <bahador.jorjani@gmail.com>
2025-05-11 10:48:45 -0400

    2-9

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,22 @@
-const Users = () => {
-  return <div>Users</div>;
+import { lightningCssTransform } from "next/dist/build/swc/generated-native";
+
+interface User {
+  id: number;
+  name: string;
+}
+const Users = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {});
+  const users: User[] = await res.json();
+  return (
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}{" "}
+      </ul>
+    </>
+  );
 };
 
 export default Users;
