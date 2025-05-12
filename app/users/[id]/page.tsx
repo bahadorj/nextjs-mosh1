c3cad1bcc65039316703135ebdd63152a669ed09
bahadorj <bahador.jorjani@gmail.com>
2025-05-12 04:11:26 -0400

    4-3

---
+++ app/users/[id]/page.tsx
@@ -0,0 +1,8 @@
+interface Props {
+  params: { id: number };
+}
+const UserDetailPage = ({ params: { id } }: Props) => {
+  return <div>UserDetailPage {id}</div>;
+};
+
+export default UserDetailPage;
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,30 +1,9 @@
-interface User {
-  id: number;
-  name: string;
-  email: string;
-}
-const Users = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users", {});
-  const users: User[] = await res.json();
+import UserTable from "./UserTable";
+const Users = () => {
   return (
     <>
       <h1>Users</h1>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <UserTable />
     </>
   );
 };
