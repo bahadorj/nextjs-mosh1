2e60845c945fc397fb41688774a978e8c2de83f9
bahadorj <bahador,jorjani@gmail.com>
2025-05-15 15:16:45 -0400

    5-7

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "../schema";
 interface Props {
   params: { id: number };
 }
@@ -11,8 +12,11 @@ export function GET(request: NextRequest, { params: { id } }: Props) {
 
 export async function PUT(request: NextRequest, { params: { id } }: Props) {
   const body = await request.json();
-  if (!body.name)
-    return NextResponse.json({ erroe: "Name is required" }, { status: 400 });
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  // if (!body.name)
+  //   return NextResponse.json({ erroe: "Name is required" }, { status: 400 });
   if (id > 10)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   return NextResponse.json({ id: 1, name: body.name });
