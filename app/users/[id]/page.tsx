702787ae6dec5d79aa1536dd6562bfaad48ed0a4
bahadorj <bahador,jorjani@gmail.com>
2025-05-15 11:08:47 -0400

    4-10

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,7 +1,10 @@
+import { notFound } from "next/navigation";
+
 interface Props {
   params: { id: number };
 }
 const UserDetailPage = ({ params: { id } }: Props) => {
+  if (id > 10) notFound();
   return <div>UserDetailPage {id}</div>;
 };
 
